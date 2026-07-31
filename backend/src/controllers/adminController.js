4c52326f2efb173982c17620f3596afd698b4164
arjunsan22 <arjunanil2114@gmail.com>
2026-07-31 12:40:23 +0530

    now visitor filtering ,sorting , pagination , search, count all completed

--- backend/src/controllers/adminController.js
+++ backend/src/controllers/adminController.js
@@ -1,5 +1,5 @@
 import { getDashboardStats } from "../models/Admin.js";
-
+import { getVisitors as getVisitorsModel } from "../models/Visitor.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 
@@ -12,6 +12,38 @@ export const dashboardStats = asyncHandler(async (req, res) => {
             200,
             stats,
             "Dashboard statistics fetched successfully"
+        )
+    );
+
+});
+
+
+//////\\\\\\\\visitors search filtering sorting pagination///\\\\\\\
+export const getVisitors = asyncHandler(async (req, res) => {
+
+    const {
+        page = 1,
+        limit = 10,
+        search = "",
+        department = "",
+        status = "",
+        visit_date = "",
+    } = req.query;
+
+    const visitors = await getVisitorsModel({
+        page,
+        limit,
+        search,
+        department,
+        status,
+        visit_date,
+    });
+
+    return res.status(200).json(
+        new ApiResponse(
+            200,
+            visitors,
+            "Visitors fetched successfully"
         )
     );
 
