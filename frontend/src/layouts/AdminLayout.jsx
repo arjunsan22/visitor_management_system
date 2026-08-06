26f262caf823b5edee3094c974e6b584c3dd4288
arjunsan22 <arjunanil2114@gmail.com>
2026-08-06 13:44:51 +0530

    outlets completed Layout files are created with outlets

---
+++ frontend/src/layouts/AdminLayout.jsx
@@ -0,0 +1,12 @@
+
+import { Outlet } from "react-router-dom";
+
+export const AdminLayout = () => {
+    return (
+        <>
+            <h2>Admin Layout</h2>
+
+            <Outlet />
+        </>
+    );
+};
---
+++ frontend/src/layouts/SecurityLayout.jsx
@@ -0,0 +1,12 @@
+
+import { Outlet } from "react-router-dom";
+
+export const SecurityLayout = () => {
+    return (
+        <>
+            <h2>Security Layout</h2>
+
+            <Outlet />
+        </>
+    );
+};
