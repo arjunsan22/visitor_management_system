08c0e95ef8b5f97232dc9e587bc49d17ea34197c
arjunsan22 <arjunanil2114@gmail.com>
2026-08-08 19:21:51 +0530

    frontend desktop testing file for visitor created response in it

---
+++ frontend/testing.js
@@ -0,0 +1,9 @@
+// {
+//   "success": true,
+//   "statusCode": 201,
+//   "message": "Visitor pass created successfully",
+//   "data": {
+//     "visitorId": 8,
+//     "pass_token": "9db9dce2-ef54-44a7-8a6e-0c6728475cb8"
+//   }
+// }
