e4381ab9a7d4d3c5d6c1cd6c9c786bd7f8551655
arjunsan22 <arjunanil2114@gmail.com>
2026-08-09 17:23:10 +0530

    security scanner page for scan the visitor details started 1st try

--- frontend/src/pages/security/Scanner.jsx
+++ frontend/src/pages/security/Scanner.jsx
@@ -1,7 +1,56 @@
-import React from 'react'
+import { Scanner as QRScanner } from "@yudiel/react-qr-scanner";
+import { useNavigate } from "react-router-dom";
 
 export const Scanner = () => {
+
+  const navigate = useNavigate();
+
+  const handleScan = (result) => {
+
+    if (!result || result.length === 0) {
+      return;
+    }
+
+    const scannedValue = result[0].rawValue;
+
+    console.log("Scanned QR:", scannedValue);
+
+    try {
+
+      const scannedUrl = new URL(scannedValue);
+
+      const pathParts = scannedUrl.pathname.split("/");
+
+      const token = pathParts[pathParts.length - 1];
+
+      if (!token) {
+        console.error("Invalid visitor QR code");
+        return;
+      }
+
+      console.log("Visitor Pass Token:", token);
+
+      navigate(`/security/visitor/${token}`);
+
+    } catch (error) {
+
+      console.error(
+        "Invalid QR code:",
+        error
+      );
+
+    }
+  };
+
   return (
-    <div>Scanner</div>
-  )
-}
+    <div>
+
+      <h1>Scan Visitor Pass</h1>
+
+      <QRScanner
+        onScan={handleScan}
+      />
+
+    </div>
+  );
+};
