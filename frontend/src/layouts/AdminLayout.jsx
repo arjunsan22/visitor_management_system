4179b20bc8f6d6b71e4faf942f136471184b0aff
arjunsan22 <arjunanil2114@gmail.com>
2026-09-12 19:18:05 +0530

    some changes in adminlayout

--- frontend/src/layouts/AdminLayout.jsx
+++ frontend/src/layouts/AdminLayout.jsx
@@ -4,8 +4,6 @@ import { Outlet } from "react-router-dom";
 export const AdminLayout = () => {
     return (
         <>
-            <h2>Admin Layout</h2>
-
             <Outlet />
         </>
     );
