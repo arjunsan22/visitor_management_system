bbd6658c971dd3bce4bd24e24731c615af0f9649
arjunsan22 <arjunanil2114@gmail.com>
2026-09-12 19:27:17 +0530

    some changes in header

--- frontend/src/components/visitor/Header.jsx
+++ frontend/src/components/visitor/Header.jsx
@@ -14,11 +14,11 @@ export const Header = () => {
                     /> */}
 
                     {/* Mobile NIT Logo */}
-<img
-    src="/white-nitc-logo.png"
-    alt="NIT Calicut"
-    className="h-14 w-auto object-contain sm:hidden"
-/>
+                    <img
+                        src="/white-nitc-logo.png"
+                        alt="NIT Calicut"
+                        className="h-14 w-auto object-contain sm:hidden"
+                    />
 
                     {/* Desktop NIT Logo */}
                     <img
@@ -27,15 +27,6 @@ export const Header = () => {
                         className="hidden w-auto object-contain sm:block sm:h-14 md:h-16"
                     />
 
-                    <div className="hidden sm:block h-8 w-[1px] bg-gray-300"></div>
-                    <div className="flex flex-col text-left">
-                        <div className="flex items-center gap-2">
-                            <span className="hidden sm:inline-block h-2 w-2 rounded-full bg-blue-600 animate-pulse"></span>
-                            <h1 className="hidden sm:inline text-sm font-bold  text-white sm:text-xl md:text-2xl sm:text-gray-900 font-nagaiya uppercase">
-                                Visitor Management System - work in progress
-                            </h1>
-                        </div>
-                    </div>
                 </div>
 
             </div>
